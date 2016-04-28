8a29ec5ce6cb194ea942f046c64cc0b5ec76446b
Danny Luong <dannyluong408@gmail.com>
2016-04-27 19:02:04 -0700

    Updated

--- ChessConsoleClient/main.cpp
+++ ChessConsoleClient/main.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-
+//Disables echo for password
 void echo( bool on = true ){
   DWORD  mode;
   HANDLE hConIn = GetStdHandle( STD_INPUT_HANDLE );
@@ -21,17 +21,26 @@ void echo( bool on = true ){
   SetConsoleMode( hConIn, mode );
 }
 
+//Eats newline from fgets
+void chomp(char *s) {
+    while(*s && *s != '\n') s++;
+    *s = 0;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    string name, password, command, temp = "login approved";
+    //Declare variables here
+    string temp = "Login Sucessful!";
     bool quit = false, admin = false;
-    char buffer[2000];
+    char buffer[2000], command[2000], user[100], password[100];
 
+    //TCP Connection
     QTcpSocket *socket = new QTcpSocket();
-    socket->connectToHost("sniperdad.com", 4000);
+    socket->connectToHost("sniperdad.com", 4001);
 
+    //Checks for connection
     if(!socket->waitForConnected(5000))
     {
         qDebug() << "Error: " << socket->errorString();
@@ -41,9 +50,11 @@ int main(int argc, char *argv[])
     else{
         cout << "Connected to server!" << endl;
     }
+
     cout << "Welcome Guest, type \"help\" for a list of commands\n";
 
     while(!quit){
+
         //Test Admin Stuff, Probably move to server side
         if (admin == false) {
             cout << "<Guest>";
@@ -53,31 +64,40 @@ int main(int argc, char *argv[])
         }
         //End Test admin Stuff
 
-        cin >> command;
+        //Grabs user input
+        fseek(stdin,0,SEEK_END); //Resets stdin to beginning
+        fgets(command,sizeof(command),stdin); // Grabs whole line of command
+        chomp(command); // Removes newline from command
 
-        if (command == "quit"){
+        //Client side commands
+        if (strcmp (command , "quit") == 0){
             quit = true;
             break;
         }
-        if (command == "login"){
+        else if (strcmp (command , "login") == 0){
             cout << "Enter your username:";
-            cin >> name;
+            cin >> user;
             cout << "Enter your password:";
             echo(false);
             cin >> password;
             echo(true);
-            command = "login "+ name + " " + password;
+            strcpy(command,"login ");
+            strcat(command,user);
+            strcat(command," ");
+            strcat(command,password);
             cout << endl;
         }
 
-        socket->write(command.c_str());
+//      cout << ":" << command << ":" << endl; //Test Stuff: Shows what were sending to socket
+
+        //sends data to socket and waits for response
+        socket->write(command);
         socket->flush();
         socket->waitForReadyRead(-1);
         socket->read(buffer, sizeof(buffer));
         cout << buffer << "\n";
 
         //Test Admin Stuff, Probably move to server side
-
         if (strcmp( buffer , temp.c_str()) == 0){
             admin = true;
         }
